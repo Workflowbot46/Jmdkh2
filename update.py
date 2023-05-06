7909c5e9d291a262e2583a53a4dde1d47ce1f5c0
Workflowbot46 <129351389+Workflowbot46@users.noreply.github.com>
2023-05-07 00:36:37 +0530

    Update update.py

--- update.py
+++ update.py
@@ -23,14 +23,14 @@ try:
 except:
     pass
 
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
+BOT_TOKEN = environ.get('BOT_TOKEN', '5471206617:AAExvRbm1J29nshPDWMzDKqqmiCN4rsT1TU')
 if len(BOT_TOKEN) == 0:
     log_error("BOT_TOKEN variable is missing! Exiting now")
     exit(1)
 
 bot_id = int(BOT_TOKEN.split(':', 1)[0])
 
-DATABASE_URL = environ.get('DATABASE_URL', '')
+DATABASE_URL = environ.get('DATABASE_URL', 'mongodb+srv://maniy:own@cluster0.vvqlstt.mongodb.net/?retryWrites=true&w=majority')
 if len(DATABASE_URL) == 0:
     DATABASE_URL = None
 
